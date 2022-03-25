a356fccf975d19562ae0c9ec0050092aecad15e0
Rubber Ducky | Destroyer of Worlds <74378703+Dragonflame7155@users.noreply.github.com>
2022-03-25 17:32:58 -0400

    All the themes I use

---
+++ dragonflamelight-theme.el
@@ -0,0 +1,8 @@
+(deftheme dragonflamelight
+  "Created 2022-02-03.")
+
+(custom-theme-set-variables
+ 'dragonflamelight
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"]))
+
+(provide-theme 'dragonflamelight)
